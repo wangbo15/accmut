519a7ad0a01f9196ef7fc66167c7a1b7a1f2665a
wangbo15 <wangbopku15@gmail.com>
2016-04-11 15:27:19 +0800

    refactor accmut_io.h and accmut_config.h

--- tools/accmut/include/accmut_config.h
+++ tools/accmut/include/accmut_config.h
@@ -18,6 +18,8 @@
 
 #include <fcntl.h>
 
+
+//USELESS SWITCH
 #define ACCMUT_GEN_MUT 0
 
 //SWITCH FOR MUTATION SCHEMATA
@@ -32,18 +34,7 @@
 #define ACCMUT_DYNAMIC_ANALYSIS_FORK 1
 
 
-
-/*************************************************/
-
-//4M buffer 
-#define MAXBUFFERSIZE 1<<22
-#define MAXMUTNUM 10000
-
-/*************************************************/
-
-
 const char PROJECT[]="printtokens";
-const char ORACLEDIR[] = "/home/nightwish/workspace/testing/accmut/printtokens/oracle/";
 int MUTATION_ID = 0;
 int TEST_ID = -1;
 
@@ -59,44 +50,19 @@ struct timeval tv_begin, tv_end;
 struct rusage usage_fbegin, usage_fmid,usage_fend;
 struct rusage usage_cbegin, usage_cend;
 
-/*************************************************/
+/**********************************************************/
 //FOR STATISTICS
 unsigned long long EXEC_INSTS = 0;
+/**********************************************************/
 
 
-/*************************************************/
 
-//FOR BUFFER
-char STDBUFF[MAXBUFFERSIZE];
-char *ORACLEBUFF;
-size_t ORACLESIZE = 0;
-size_t CURBUFSIZE = 0;
-
-/*************************************************/
+#include <accmut/accmut_io.h>
 
 
-void __accmut__oracledump();
-void __accmut__bufferdump();
 
-int __accmut__checkoutput(){
-	if(ORACLESIZE != CURBUFSIZE){
-		return 1;
-	}
-	return memcmp(STDBUFF, ORACLEBUFF, CURBUFSIZE);
-}
-
-void __accmut__exit_check(){
-	int res = __accmut__checkoutput();
-	if(res != 0){
-		fprintf(stderr, "TEST: %d KILL MUT: %d\n", TEST_ID, MUTATION_ID);
-		//__accmut__bufferdump();
-		
-	}
-
-	// if(MUTATION_ID == 0){
-	// 	__accmut__oracledump();
-	// }
-}
+/************* ALL EXIT HANDLER ***************************/
+void __accmut__exit_check_output();
 
 void __accmut__exit_time(){
 	gettimeofday(&tv_end, NULL);
@@ -130,72 +96,9 @@ void __accmut__exit_preciese_time(){
 			child_u_sec, child_u_usec, child_s_sec, child_s_usec);
 	}
 
-
-
-
 }
 
-//ORI fprintf
-// int __accmut__fprintf(FILE *stream, const char *format, ...){
-// 	int ret;
-// 	va_list ap;
-// 	va_start(ap, format);
-// 	ret = vfprintf(stream, format, ap);
-// 	va_end(ap);
-// 	return 0;
-// }
-
-int __accmut__fprintf(FILE *stream, const char *format, ...){
-	int ret;
-	va_list ap;
-	va_start(ap, format);
-	char tmp[4096] = {0};
-	ret = vsprintf(tmp, format, ap);
-	va_end(ap);
-
-	if(ret > 4096){
-		perror("BUFFER LINE OVERFLOW !!!!!!!\n");
-	}
-
-	int total = CURBUFSIZE + ret;
-	if(total >= MAXBUFFERSIZE){
-		perror("ACCMUT BUFFER OVERFLOW !!!!!!\n");
-		exit(0);
-	}
-	memcpy((STDBUFF + CURBUFSIZE), tmp, ret*(sizeof(char)) );
-	CURBUFSIZE = total;
-	return ret;
-}
-
-void __accmut__bufinit(){
-	char path[120];
-	sprintf(path, "%st%d", ORACLEDIR, TEST_ID);
-	int fd = open(path, O_RDONLY);
-	if(fd == -1){
-		fprintf(stderr, "ORACLEDIR OPEN ERROR !!!!!!\n");
-		fprintf(stderr, "ORACLEDIR PATH : %s\n", path);
-	}
-	struct stat sb;
-	if(fstat(fd, &sb) == -1){
-		fprintf(stderr, "fstat ERROR !!!!!!\n");
-	}
-	ORACLEBUFF = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-	if(ORACLEBUFF == MAP_FAILED){
-		fprintf(stderr, "mmap ERROR !!!!!!\n");
-	}
-	ORACLESIZE = sb.st_size;
-
-	//regist the exit handler function of a process
-	if(atexit(__accmut__exit_check) != 0){
-		fprintf(stderr, "__accmut__exit_check REGSITER ERROR\n");
-	}
-
-	// if(atexit(__accmut__exit_time) != 0){
-	// 	fprintf(stderr, "__accmut__exit_time REGSITER ERROR\n");
-	// }
-}
-
-
+/* The signal handler of time out process */
 void __accmut__handler(int sig){
     // fprintf(stderr, "%s", MUTSTXT[MUTATION_ID]);
     // fprintf(stderr, "%d\n", MUTATION_ID);	// TODO::stdout or stderr
@@ -203,46 +106,17 @@ void __accmut__handler(int sig){
     exit(0);
 }
 
-void __accmut__setout(int id){
- 	char path[120];
-	strcpy(path, getenv("HOME"));
-	strcat(path, "/tmp/accmut/output/");
-	strcat(path, PROJECT);
-	strcat(path, "/t");
-	sprintf(path, "%s%d_%d", path, TEST_ID, id);
-	//printf("PATH : %s\n", path);
-	if (freopen(path, "w", stdout)==NULL){
-		fprintf(stderr, "STDOUT REDIR ERR! : %s\n", path);
-	}
-}
-
-void __accmut__oracledump(){
-	fprintf(stderr, "\n********** TID:%d  ORI BUFFER SIZE: %d ***********\n", TEST_ID, ORACLESIZE);
-	int i;
-	for(i = 0; i < ORACLESIZE; i++){
-		fprintf(stderr, "%c", ORACLEBUFF[i]);
-	}
-	fprintf(stderr, "************ END OF ORACLE ***************\n\n");
-}
-
-void __accmut__bufferdump(){
-	fprintf(stderr, "\n********** TID:%d  MID:%d  ACCMUT BUF SIZE:%d ***********\n", TEST_ID, MUTATION_ID, CURBUFSIZE);
-	int i;
-	for(i = 0; i < CURBUFSIZE; i++){
-		fprintf(stderr, "%c", STDBUFF[i]);
-	}
-	fprintf(stderr, "************ END OF ACCMUT BUFFER ***************\n\n");
-}
-
-
 void __accmut__sepcific_timer(){
 	char path[100];
 	strcpy(path, getenv("HOME"));
 	strcat(path, "/tmp/accmut/oritime.txt");
 	FILE * fp = fopen(path, "r");
 	if(fp == NULL){
-		fprintf(stderr, "ORI TIME CANNOT OPEN : %s\n", path);
-		exit(0);
+		fprintf(stderr, "ORI TIME FILE DOSE NOT EXISIT : %s\n", path);
+		//if the ori time file does not exisit, use the default timer value.
+		VALUE_SEC = 0;
+		VALUE_USEC = 120;
+		//exit(0);
 	}
 	//TODO::optimize
 	int i;
--- tools/accmut/include/accmut_io.h
+++ tools/accmut/include/accmut_io.h
@@ -10,10 +10,26 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
-
+/*************************************************/
+//INPUT BUF
 #define ACCMUT_MAX_FILENO 16
 #define IO_BUF_SIZE 8192
+
+//4M OUTPUT BUF 
+#define MAXBUFFERSIZE 1<<22
+#define MAXMUTNUM 10000
+/*************************************************/
+
+
+/***********************************************************/
+//FOR OUTPUT BUFFER
+const char ORACLEDIR[] = "/home/nightwish/workspace/testing/accmut/printtokens/oracle/";
+
+char STDBUFF[MAXBUFFERSIZE];
+char *ORACLEBUFF;
+size_t ORACLESIZE = 0;
+size_t CURBUFSIZE = 0;
+/***********************************************************/
 
 
 typedef struct _ACCMUT_INPUT_FILE{
@@ -25,11 +41,9 @@ typedef struct _ACCMUT_INPUT_FILE{
 	
 }ACCMUT_FILE;
 
-
+/*********************** INPUT ****************************************/
 
 ACCMUT_FILE* __accmut__fopen(const char *path, const char *mode){
-
-
 	//TODO: check mode
 	if(*mode != 'r' && *(mode+1) != NULL){// only support READ-ONLY mode now
 		fprintf(stderr, "ERROR FOPEN MODE : %s !\n", mode);
@@ -95,5 +109,125 @@ int __accmut__fclose(ACCMUT_FILE *fp){
 int __accmut__fseek(ACCMUT_FILE *fp, size_t offset, int loc){
 	return 0;
 }
+
+
+/*********************** OUTPUT ****************************************/
+
+//ORI fprintf
+// int __accmut__fprintf(FILE *stream, const char *format, ...){
+// 	int ret;
+// 	va_list ap;
+// 	va_start(ap, format);
+// 	ret = vfprintf(stream, format, ap);
+// 	va_end(ap);
+// 	return 0;
+// }
+
+int __accmut__fprintf(FILE *stream, const char *format, ...){
+	int ret;
+	va_list ap;
+	va_start(ap, format);
+	char tmp[4096] = {0};
+	ret = vsprintf(tmp, format, ap);
+	va_end(ap);
+
+	if(ret > 4096){
+		perror("BUFFER LINE OVERFLOW !!!!!!!\n");
+	}
+
+	int total = CURBUFSIZE + ret;
+	if(total >= MAXBUFFERSIZE){
+		perror("ACCMUT BUFFER OVERFLOW !!!!!!\n");
+		exit(0);
+	}
+	memcpy((STDBUFF + CURBUFSIZE), tmp, ret*(sizeof(char)) );
+	CURBUFSIZE = total;
+	return ret;
+}
+
+
+void __accmut__bufferdump();
+void __accmut__oracledump();
+
+int __accmut__checkoutput(){
+	if(ORACLESIZE != CURBUFSIZE){
+		return 1;
+	}
+	return memcmp(STDBUFF, ORACLEBUFF, CURBUFSIZE);
+}
+
+void __accmut__exit_check_output(){
+	int res = __accmut__checkoutput();
+	if(res != 0){
+		fprintf(stderr, "TEST: %d KILL MUT: %d\n", TEST_ID, MUTATION_ID);
+		//__accmut__bufferdump();
+		
+	}
+
+	// if(MUTATION_ID == 0){
+	// 	__accmut__oracledump();
+	// }
+}
+
+void __accmut__bufinit(){
+	char path[120];
+	sprintf(path, "%st%d", ORACLEDIR, TEST_ID);
+	int fd = open(path, O_RDONLY);
+	if(fd == -1){
+		fprintf(stderr, "ORACLEDIR OPEN ERROR !!!!!!\n");
+		fprintf(stderr, "ORACLEDIR PATH : %s\n", path);
+	}
+	struct stat sb;
+	if(fstat(fd, &sb) == -1){
+		fprintf(stderr, "fstat ERROR !!!!!!\n");
+	}
+	ORACLEBUFF = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if(ORACLEBUFF == MAP_FAILED){
+		fprintf(stderr, "mmap ERROR !!!!!!\n");
+	}
+	ORACLESIZE = sb.st_size;
+
+	//regist the exit handler function of a process
+	if(atexit(__accmut__exit_check_output) != 0){
+		fprintf(stderr, "____accmut__exit_check_output REGSITER ERROR\n");
+		exit(0);
+	}
+
+	// if(atexit(__accmut__exit_time) != 0){
+	// 	fprintf(stderr, "__accmut__exit_time REGSITER ERROR\n");
+	// }
+}
+
+void __accmut__setout(int id){
+ 	char path[120];
+	strcpy(path, getenv("HOME"));
+	strcat(path, "/tmp/accmut/output/");
+	strcat(path, PROJECT);
+	strcat(path, "/t");
+	sprintf(path, "%s%d_%d", path, TEST_ID, id);
+	//printf("PATH : %s\n", path);
+	if (freopen(path, "w", stdout)==NULL){
+		fprintf(stderr, "STDOUT REDIR ERR! : %s\n", path);
+	}
+}
+
+void __accmut__oracledump(){
+	fprintf(stderr, "\n********** TID:%d  ORI BUFFER SIZE: %d ***********\n", TEST_ID, ORACLESIZE);
+	int i;
+	for(i = 0; i < ORACLESIZE; i++){
+		fprintf(stderr, "%c", ORACLEBUFF[i]);
+	}
+	fprintf(stderr, "************ END OF ORACLE ***************\n\n");
+}
+
+void __accmut__bufferdump(){
+	fprintf(stderr, "\n********** TID:%d  MID:%d  ACCMUT BUF SIZE:%d ***********\n", TEST_ID, MUTATION_ID, CURBUFSIZE);
+	int i;
+	for(i = 0; i < CURBUFSIZE; i++){
+		fprintf(stderr, "%c", STDBUFF[i]);
+	}
+	fprintf(stderr, "************ END OF ACCMUT BUFFER ***************\n\n");
+}
+
 
 #endif
